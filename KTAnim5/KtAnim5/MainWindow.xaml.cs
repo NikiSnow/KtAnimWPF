9de97ee4c4f9170ea11fa749ddbf7fe86cc90d31
NikiSnow <119755929+NikiSnow@users.noreply.github.com>
2025-05-27 03:38:29 +0300

    Add files via upload

---
+++ KTAnim5/KtAnim5/MainWindow.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows;
+using System.Windows.Media.Animation;
+
+namespace KtAnim5
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void StartAnimations_Click(object sender, RoutedEventArgs e)
+        {
+            // Останавливаем предыдущие анимации, если они были
+            (FindResource("PathAnimation") as Storyboard)?.Begin(this, true);
+            (FindResource("FadeAnimation") as Storyboard)?.Begin(this, true);
+        }
+    }
+
+
+}
