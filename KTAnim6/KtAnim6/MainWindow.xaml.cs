3638262ebeaadaf690689e76411c0557ac780733
NikiSnow <119755929+NikiSnow@users.noreply.github.com>
2025-05-27 03:39:02 +0300

    Add files via upload

---
+++ KTAnim6/KtAnim6/MainWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows;
+using System.Windows.Media.Animation;
+
+namespace KtAnim6
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void MenuButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (menuPanel.Visibility == Visibility.Collapsed)
+            {
+                Storyboard openAnimation = (Storyboard)FindResource("OpenMenuAnimation");
+                openAnimation.Begin(this);
+            }
+            else
+            {
+                Storyboard closeAnimation = (Storyboard)FindResource("CloseMenuAnimation");
+                closeAnimation.Begin(this);
+            }
+        }
+    }
+}
